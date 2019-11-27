378b1dc5aaf51c1ddf35d624fd9aa86c9c9d8d52
Putilov Aleksandr <pai@tochka.com>
2019-11-27 16:45:30 +0500

    add types for addons

--- addons/dist/createNumberMask.d.ts
+++ addons/dist/createNumberMask.d.ts
@@ -1,2 +1,16 @@
-declare var foo: any
-export default foo
+export interface ICreateNumberMaskConfig {
+    prefix?: string;
+    suffix?: string;
+    includeThousandsSeparator?: boolean;
+    thousandsSeparatorSymbol?: string;
+    allowDecimal?: boolean;
+    decimalSymbol?: string;
+    decimalLimit?: number;
+    requireDecimal?: boolean;
+    allowNegative?: boolean;
+    allowLeadingZeroes?: boolean;
+    integerLimit?: number | null;
+    maxValue: number;
+    minValue: number;
+}
+export default function createNumberMask(config: ICreateNumberMaskConfig): (rawValue: string) => Array<string>
